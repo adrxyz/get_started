cb40bcad1d847d34bda39c1fccc8df7fecf15b49
Adriel Montejo <147715889+adrxyz@users.noreply.github.com>
2025-09-14 23:12:16 +0700

    Nightmare ahhh frontendcode

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:get_started/sign_in/sign_in.dart';
+import 'package:get_started/screens/onboarding_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  // Ensure that Flutter widgets are initialized before Firebase.
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase with the default options.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   runApp(const MyApp());
 }
 
@@ -17,11 +21,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sign In App',
+      title: 'Onboarding UI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
       ),
-      home: const SignInScreen(),
+      home: const OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
